e1c00f74cf45888ecd4b41811ec309cf6f2cfb79
sabalj <43485145+sabalj@users.noreply.github.com>
2018-09-24 16:15:48 -0700

    Add files via upload

--- planets.py
+++ planets.py
@@ -1,7 +1,23 @@
+#
+#Name: Sabal Jayaswal
+#Student ID: 014331872
+#Date (last modified):9/24/2018
+#
+# Lab 0
+# Section 14
+# Purpose of Lab: To calculate the user's weight on Mars and Jupiter
+# additional comments
+
+
+
 def weight_on_planets():
-   # write your code here
-   
-   
-   
+    user_weight = float(input("What do you weigh on earth? "))
+    mars_weight = user_weight*0.38
+    jupiter_weight = user_weight*2.34
+    print ("\nOn Mars you would weigh",  mars_weight, "pounds.\n" +
+           "On Jupiter you would weigh" , jupiter_weight , "pounds.")
+
+
+
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
